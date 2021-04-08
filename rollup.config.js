0656d89c752a7bd514f56ee2a79595af06199cfe
YONGLONG <YONG@163.COM>
2021-04-08 17:02:04 +0800

    doc (添加注释) 配置注释

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,14 @@
+/** 此插件可以告诉rollup 如何查找外部模块 */
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+
+// rollup-plugin-babel 使用 Babel 和 Rollup 的最简单方法是使用 [rollup-plugin-babel]
+import babel from "rollup-plugin-babel";
+
+/** typeScript rollup 插件 */
 import typescript from "rollup-plugin-typescript";
+
+/** 包信息 */
 import pkg from "./package.json";
 
 export default [
